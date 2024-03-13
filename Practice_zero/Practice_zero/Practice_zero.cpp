fea6a39d64499267640369f09fd657202dd30228
AntonZenin <zenin.anton.2005@gmail.com>
2024-03-13 11:06:56 +0300

    Some changes

--- Practice_zero/Practice_zero/Practice_zero.cpp
+++ Practice_zero/Practice_zero/Practice_zero.cpp
@@ -4,10 +4,17 @@
 int main()
 {
     TVector v1, v2;
+    int n;
     std::cin >> v1 >> v2;
-    TVector res = v1 + v2; 
+    TVector res1 = v1 + v2; 
+    TVector res2 = v1 - v2;
+    TVector res3 = v1 * v2;
+    
     std::cout << v1;
     std::cout << v2;
-    std::cout << res;
+    std::cout << res1;
+    std::cout << res2;
+    std::cout << res3;
+    
     return 0;
 }
