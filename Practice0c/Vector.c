c19be9560cfb6b666c955de4319ed4209fee8429
AntonZenin <zenin.anton.2005@gmail.com>
2024-03-24 14:53:10 +0300

    Finished c

--- Practice0c/Vector.c
+++ Practice0c/Vector.c
@@ -1,140 +1,122 @@
 #include "Vector.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 
 void alloc(TVector* v, int n)
 {
-	v->n = n;
-	v->x = (double*)malloc(sizeof(double) * n);
+    v->n = n;
+    v->x = (double*)malloc(sizeof(double) * n);
 }
 
-void fill(TVector* v)
+TVector summ(TVector* v1, TVector* v2)
 {
-	int i = 0;
-	printf("Input coordinates: ");
-	for (; i < v->n; i++)
-	{
-		scanf("%lf", &(v->x[i]));
-	}
+    TVector sum;
+    int i = 0;
+    if (v1->n != v2->n)
+    {
+        printf("ERROR!");
+        sum.x = NULL;
+        sum.n = 0;
+        return sum;
+    }
+    alloc(&sum, v1->n);
+    for (i = 0; i < sum.n; i++)
+    {
+        sum.x[i] = v1->x[i] + v2->x[i];
+    }
+    return sum;
 }
 
-void sum_print(TVector* v)
+void fill(const char* filename, TVector* v1, TVector* v2)
 {
-	int i = 0;
-	printf("Vector sum: ");
-	for (; i < v->n; i++)
-	{
-		printf("%.2lf ", v->x[i]);
-	}
-	printf("\n");
+    int i = 0;
+    FILE* f = fopen(filename, "r");
+    if (f == NULL)
+    {
+        printf("We didn't find file");
+        abort();
+    }
+    fscanf(f, "%d", &(v1->n));
+    v1->x = (double*)malloc(v1->n * sizeof(double));
+    for (; i < (v1->n); i++)
+    {
+        fscanf(f, "%lf", &(v1->x[i]));
+    }
+    fscanf(f, "%d", &(v2->n));
+    v2->x = (double*)malloc(v2->n * sizeof(double));
+    for (i = 0; i < (v2->n); i++)
+    {
+        fscanf(f, "%lf", &(v2->x[i]));
+    }
+    fclose(f);
 }
 
-void diff_print(TVector* v)
+void write(const char* filename, TVector* sum, TVector* sub, double* mult)
 {
-	int i = 0;
-	printf("Vector dif: ");
-	for (; i < v->n; i++)
-	{
-		printf("%.2lf ", v->x[i]);
-	}
-	printf("\n");
+    int i = 0;
+    FILE* f = fopen(filename, "w+");
+    if (f == NULL)
+    {
+        printf("We didn't find file");
+        abort();
+    }
+    fprintf(f, "Summ is ");
+    for (; i < sum->n; i++)
+    {
+        fprintf(f, "%lf ", sum->x[i]);
+    }
+    fprintf(f, "\nDiff is ");
+    for (i = 0; i < sub->n; i++)
+    {
+        fprintf(f, "%lf ", sub->x[i]);
+    }
+    fprintf(f, "\nMult is %lf\n", *mult);
+    fclose(f);
 }
 
-TVector sum(TVector* v1, TVector* v2)
+void print(TVector* v)
 {
-	TVector res;
-	int i = 0;
-	if (v1->n != v2->n)
-	{
-		printf("v1->n != v2->n");
-		res.x = NULL;
-		res.n = 0;
-		return res;
-	}
-	alloc(&res, v1->n);
-	for (; i < res.n; i++)
-	{
-		res.x[i] = v1->x[i] + v2->x[i];
-	}
-	return res;
+    int i = 0;
+    for (i = 0; i < v->n; i++)
+    {
+        printf("%lf ", v->x[i]);
+    }
+    printf("\n");
 }
 
 TVector diff(TVector* v1, TVector* v2)
 {
-	TVector res;
-	int i = 0;
-	if (v1->n != v2->n)
-	{
-		printf("v1->n != v2->n");
-		res.x = NULL;
-		res.n = 0;
-		return res;
-	}
-	alloc(&res, v1->n);
-	for (; i < res.n; i++)
-	{
-		res.x[i] = v1->x[i] - v2->x[i];
-	}
-	return res;
+    TVector dif;
+    int i = 0;
+    if (v1->n != v2->n)
+    {
+        printf("ERROR!");
+        dif.x = NULL;
+        dif.n = 0;
+        return dif;
+    }
+    alloc(&dif, v1->n);
+    for (i = 0; i < dif.n; i++)
+    {
+        dif.x[i] = v1->x[i] - v2->x[i];
+    }
+    return dif;
 }
 
 double mult(TVector* v1, TVector* v2)
 {
-	int i = 0;
-	double res = 0;
-	if (v1->n != v2->n)
-	{
-		printf("v1->n != v2->n");
-		return 0;
-	}
-	for (; i < v1->n; i++)
-	{
-		res = res + ((v1->x[i]) * (v2->x[i]));
-	}
-	return res;
-}
-
-void reading(const char* filename, TVector* v1, TVector* v2)
-{
-	int i = 0;
-	FILE* f = fopen(filename, "r");
-	if (f == NULL)
-	{
-		printf("File not found");
-		abort();
-	}
-	fscanf(f, " %d ", &(v1->n));
-	v1->x = (double*)malloc(v1->n * sizeof(double));
-	for (; i < (v1->n); i++)
-	{
-		fscanf(f, " %lf ", &(v1->x[i]));
-	}
-	fscanf(f, "%d ", &(v2->n));
-	v2->x = (double*)malloc(v2->n * sizeof(double));
-	for (i = 0; i < (v2->n); i++)
-	{
-		fscanf(f, "%lf ", &(v2->x[i]));
-	}
-	fclose(f);
-}
-
-void writing(const char* filename, TVector* s, TVector* d, double mp)
-{
-	int i = 0;
-	FILE* f = fopen(filename, "w+");
-	if (f == NULL)
-	{
-		printf("File not found");
-		abort();
-	}
-	fprintf(f, "Sum =");
-	for (; i < s->n; i++)
-	{
-		fprintf(f, "%.2lf  ", (s->x[i]));
-	}
-	fprintf(f, "\nDiff =");
-	for (i = 0; i < d->n; i++)
-	{
-		fprintf(f, "%.2lf  ", (d->x[i]));
-	}
-	fprintf(f, "\nScalar multiplication = %.3lf", mp);
-	fclose(f);
+    double mult = 0;
+    int i = 0;
+    if (v1->n != v2->n)
+    {
+        printf("ERROR!");
+        mult = 0;
+        return mult;
+    }
+    for (i = 0; i < v1->n; i++)
+    {
+        mult += v1->x[i] * v2->x[i];
+    }
+    return mult;
 }
--- Practice0c/Vector.h
+++ Practice0c/Vector.h
@@ -1,24 +1,19 @@
 #ifndef FUNC
 
-#define FUNC 
+#define FUNC
 
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct
-{
-	int n;
-	double* x;
-} TVector;
+typedef struct {
+    int n;
+    double* x;
+}TVector;
 
 void alloc(TVector* v, int n);
-void fill(TVector* v);
-void sum_print(TVector* v);
-void diff_print(TVector* v);
-
-TVector sum(TVector* v1, TVector* v2);
+TVector summ(TVector* v1, TVector* v2);
 TVector diff(TVector* v1, TVector* v2);
 double mult(TVector* v1, TVector* v2);
+void fill(const char* filename, TVector* v1, TVector* v2);
+void print(TVector* v);
+void write(const char* filename, TVector* sum, TVector* sub, double* mult);
 
-#endif
+#endif 
 
